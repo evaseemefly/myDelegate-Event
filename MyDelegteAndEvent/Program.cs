73901d4e8d249e87f2c78d21524f8c1960613f56
liusihan-单位 <evaseemefly@126.com>
2016-11-10 17:35:11 +0800

    创建 委托及事件的示例

---
+++ MyDelegteAndEvent/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDelegteAndEvent
+{
+    /// <summary>
+    /// 打招呼委托
+    /// </summary>
+    /// <param name="name"></param>
+    public delegate void GreatingDelegate(string name);
+    class Program
+    {
+        private static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("hello"+name);
+        }
+
+        private static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("你好" + name);
+        }
+
+        private static void GreetPeople(string name,GreatingDelegate MakeGreeting)
+        {
+            //将方法作为方法的参数传递
+            MakeGreeting(name);
+        }
+        static void Main(string[] args)
+        {
+            //1 方式1：将方法作为参数传递
+            //GreetPeople("casablanca", EnglishGreeting);
+            //GreetPeople("小王", ChineseGreeting);
+
+            //2 方式2：
+            //GreatingDelegate delegates;
+            //delegates = EnglishGreeting;
+            //delegates += ChineseGreeting;
+            ////2.1 调用方式1
+            //GreetPeople("drno", delegates);
+            ////2.2 调用方式2
+            //delegates("drno");
+
+            //3 方式3：
+            GreatingDelegate delegates1 = new GreatingDelegate(EnglishGreeting);
+            delegates1 += ChineseGreeting;
+            delegates1("drno");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyEvent/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvent
+{
+    /// <summary>
+    /// 打招呼委托
+    /// </summary>
+    /// <param name="name"></param>
+    public delegate void GreetingDelegate(string name);
+
+    public class GreetingManager
+    {
+        public void GreetPeople(string name,GreetingDelegate MakeGreeting)
+        {
+            MakeGreeting(name);
+        }
+    }
+
+    public class GreetingManager1
+    {
+        public GreetingDelegate delegate1;
+        public void GreetPeople(string name, GreetingDelegate MakeGreeting)
+        {
+            
+            MakeGreeting(name);
+        }
+
+        public void GreetPeoplebyDele(string name)
+        {
+            //若有方法注册委托变量
+            if (delegate1 != null)
+            {
+                //通过委托调用方法
+                delegate1(name);
+            }
+        }
+    }
+
+    class Program
+    {
+        private static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("hello" + name);
+        }
+
+        private static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("你好" + name);
+        }
+
+        static void Main(string[] args)
+        {
+            //1 调用GreetingManager
+            GreetingManager gm = new GreetingManager();
+            gm.GreetPeople("drno", EnglishGreeting);
+            gm.GreetPeople("小王", ChineseGreeting);
+
+            //2 调用GreetingManager1（属性为委托）
+            GreetingManager1 gm1 = new GreetingManager1();
+            gm1.delegate1 = EnglishGreeting;
+            gm1.delegate1 += ChineseGreeting;
+
+            //gm1.GreetPeople("drno", gm1.delegate1);
+
+            gm1.GreetPeoplebyDele("drno");
+
+            Console.ReadKey();
+        }
+    }
+}
